7a9a7643ae44a363fca50b28f573be65e597280a
Fromant <framanchannel@gmail.com>
2026-02-09 22:56:38 +0300

    optimize form conversion a little

--- src/FormConverter.cpp
+++ src/FormConverter.cpp
@@ -56,79 +56,10 @@ LinearProgram FormConverter::to_general_form(const LinearProgram& lp) {
     general.rhs() = std::move(new_rhs);
     general.prettierCoeffs();
 
-    // Проверка корректности ограничений на переменные
-    for (const auto& vc : general.var_constraints()) {
-        if (vc != ">=0" && vc != "free") {
-            throw std::runtime_error("Internal error: invalid variable constraint in general form: " + vc);
-        }
-    }
-
     return general;
 }
 
 LinearProgram FormConverter::to_symmetric_form(const LinearProgram& lp) {
-    const LinearProgram general = to_general_form(lp);
-
-    const auto M1 = general.getM1();
-    const auto M2 = general.getM2();
-    const auto N1 = general.getN1();
-    const auto N2 = general.getN2();
-
-    size_t original_vars = lp.num_variables();
-    size_t free_vars = N2.size();
-    size_t total_vars = original_vars + free_vars;
-
-    size_t original_constraints = general.num_constraints();
-    size_t total_constraints = original_constraints + M2.size();
-
-    std::vector<std::string> new_var_constraints(total_vars, ">=0");
-
-    // c = (c[N1], c[N2], -c[N2])
-    std::vector<double> new_objective;
-    new_objective.reserve(total_vars);
-    for (const double& i : general.objective()) {
-        new_objective.emplace_back(i);
-    }
-    for (const auto& i : N2) {
-        new_objective.emplace_back(-general.objective()[i]);
-    }
-
-    // b = (b[M1], b[M2], -b[M2])
-    std::vector<double> new_rhs;
-    new_rhs.reserve(total_constraints);
-    for (const double& i : general.rhs()) {
-        new_rhs.emplace_back(i);
-    }
-    for (const auto& i : M2) {
-        new_rhs.emplace_back(-general.rhs()[i]);
-    }
-
-    //     ( A[M1][N1],  A[M1][N2], -A[M1,N2])
-    // A = ( A[M2][N1],  A[M2][N2], -A[M2,N2])
-    //     (-A[M2][N1], -A[M2][N2],  A[M2,N2])
-    auto new_constraints = general.constraints();
-    for (size_t y = 0; y < general.constraints().size(); y++) {
-        for (const auto& i : N2) {
-            new_constraints[y].emplace_back(-general.constraints()[y][i]);
-        }
-    }
-    for (size_t y = 0; y < M2.size(); y++) {
-        new_constraints.emplace_back();
-        new_constraints[y + original_constraints].reserve(total_vars);
-        for (size_t x = 0; x < original_vars; x++) {
-            new_constraints[y + original_constraints].emplace_back(-general.constraints()[M2[y]][x]);
-        }
-        for (const auto& i : N2) {
-            new_constraints[y + original_constraints].emplace_back(general.constraints()[M2[y]][i]);
-        }
-    }
-
-    const std::vector<std::string> new_relations(total_constraints, ">=");
-
-    return {true, new_objective, new_constraints, new_relations, new_rhs, new_var_constraints};
-}
-
-LinearProgram FormConverter::to_canonical_form(const LinearProgram& lp) {
     LinearProgram general = to_general_form(lp);
 
     const auto M1 = general.getM1();
@@ -136,65 +67,121 @@ LinearProgram FormConverter::to_canonical_form(const LinearProgram& lp) {
     const auto N1 = general.getN1();
     const auto N2 = general.getN2();
 
-    size_t n = general.num_variables();
-    size_t m = general.num_constraints();
+    size_t original_vars = lp.num_variables();
+    size_t new_vars = N2.size();
+    size_t total_vars = original_vars + new_vars;
 
-    size_t n_new = n + N2.size() + M1.size();
-    size_t m_new = m;
+    size_t original_constraints = general.num_constraints();
+    size_t new_constraints = M2.size();
+    size_t total_constraints = original_constraints + new_constraints;
 
+    // c = (c[N1], c[N2], -c[N2])
+    for (const auto& i : N2) {
+        general.objective().emplace_back(-general.objective()[i]);
+    }
+
+    // b = (b[M1], b[M2], -b[M2])
+    for (const auto& i : M2) {
+        general.rhs().emplace_back(-general.rhs()[i]);
+    }
+
+
+    auto& constraints = general.constraints();
+
+    //     ( A[M1][N1],  A[M1][N2], -A[M1,N2])
+    // A = ( A[M2][N1],  A[M2][N2], -A[M2,N2])
+    //     (-A[M2][N1], -A[M2][N2],  A[M2,N2])
+    for (size_t y = 0; y < constraints.size(); y++) {
+        for (const auto& i : N2) {
+            constraints[y].emplace_back(-constraints[y][i]);
+        }
+    }
+    for (size_t y = 0; y < M2.size(); y++) {
+        constraints.emplace_back();
+        constraints[y + original_constraints].reserve(total_vars);
+        for (size_t x = 0; x < original_vars; x++) {
+            constraints[y + original_constraints].emplace_back(-constraints[M2[y]][x]);
+        }
+        for (const auto& i : N2) {
+            constraints[y + original_constraints].emplace_back(constraints[M2[y]][i]);
+        }
+    }
+
+    std::ranges::fill(general.relations(), ">=");
+    for (size_t i = 0; i < new_constraints; i++) {
+        general.relations().emplace_back(">=");
+    }
+
+    std::ranges::fill(general.var_constraints(), ">=0");
+    for (size_t i = 0; i < new_vars; i++) {
+        general.var_constraints().emplace_back(">=0");
+    }
+
+    return general;
+}
+
+LinearProgram FormConverter::to_canonical_form(const LinearProgram& lp) {
+    LinearProgram general = to_general_form(lp);
+
+    auto& constraints = general.constraints();
+    auto& rhs = general.rhs();
+
+    const auto M1 = general.getM1();
+    const auto M2 = general.getM2();
+    const auto N1 = general.getN1();
+    const auto N2 = general.getN2();
+
+    size_t new_vars = N2.size() + M1.size();
 
     // c = (c[N1], c[N2], -c[N2], 0[M1])
-    std::vector<double> new_objective;
-    new_objective.reserve(n_new);
-
-    for (size_t i = 0; i < n; i++) {
-        new_objective.emplace_back(general.objective()[i]);
-    }
     for (const auto& i : N2) {
-        new_objective.emplace_back(-general.objective()[i]);
+        general.objective().emplace_back(-general.objective()[i]);
     }
     for (int i = 0; i < M1.size(); i++) {
-        new_objective.emplace_back(0);
+        general.objective().emplace_back(0);
     }
 
     // A = (A[M1,N1], A[M1,N2], -A[M1,N2], -E[M1,M1]
     //     (A[M2,N1], A[M2,N2], -A[M2,N2],  0[M2,M1]
-
-    auto new_constraints = general.constraints();
     for (size_t j = 0; j < M1.size(); j++) {
         const size_t y = M1[j];
         for (const auto& i : N2) {
-            new_constraints[y].emplace_back(-general.constraints()[y][i]);
+            constraints[y].emplace_back(-general.constraints()[y][i]);
         }
         for (size_t i = 0; i < M1.size(); i++) {
             if (i == j) {
-                new_constraints[y].emplace_back(-1);
+                constraints[y].emplace_back(-1);
             } else {
-                new_constraints[y].emplace_back(0);
+                constraints[y].emplace_back(0);
             }
         }
     }
     for (const auto& y : M2) {
         for (const auto& i : N2) {
-            new_constraints[y].emplace_back(-general.constraints()[y][i]);
+            constraints[y].emplace_back(-general.constraints()[y][i]);
         }
         for (size_t i = 0; i < M1.size(); i++) {
-            new_constraints[y].emplace_back(0);
+            constraints[y].emplace_back(0);
         }
     }
 
-    std::vector<double> new_rhs = general.rhs();
+    std::ranges::fill(general.var_constraints(), ">=0");
+    for (size_t i = 0; i < new_vars; i++) {
+        general.var_constraints().emplace_back(">=0");
+    }
+    // now new constraints
+    std::ranges::fill(general.relations(), "=");
 
-    std::vector<std::string> new_var_constraints(n_new, ">=0");
+    // make rhs >= 0
+    for (size_t i = 0; i < general.num_constraints(); ++i) {
+        if (rhs[i] < 0) {
+            for (size_t j = 0; j < general.num_variables(); ++j) {
+                constraints[i][j] = -constraints[i][j];
+            }
+            rhs[i] = -rhs[i];
+        }
+    }
 
-    LinearProgram result;
-    result.is_minimization() = true;
-    result.objective() = std::move(new_objective);
-    result.constraints() = std::move(new_constraints);
-    result.relations() = std::vector<std::string>(m_new, "=");
-    result.rhs() = std::move(new_rhs);
-    result.var_constraints() = std::move(new_var_constraints);
-
-    result.prettierCoeffs();
-    return result;
+    general.prettierCoeffs();
+    return general;
 }
