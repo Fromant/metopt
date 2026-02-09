c4c0e81cab5eac5625292047a1fbe23ba65c8f0f
Fromant <framanchannel@gmail.com>
2026-02-09 17:39:59 +0300

    dual builder do get rid of general form constraint

--- src/DualBuilder.cpp
+++ src/DualBuilder.cpp
@@ -1,42 +1,43 @@
 #include "DualBuilder.hpp"
 #include <cmath>
-#include <stdexcept>
 #include <string>
 #include <vector>
 
-LinearProgram DualBuilder::build_dual(const LinearProgram& general) {
-    if (general.getForm() == LinearProgram::UNDEFINED) {
-        throw std::runtime_error("Dual construction error: can construct only dual to general form");
+#include "FormConverter.hpp"
+
+LinearProgram DualBuilder::build_dual(LinearProgram problem) {
+    if (problem.getForm() == LinearProgram::UNDEFINED) {
+        problem = FormConverter::to_general_form(problem);
     }
 
     // Общая форма всегда на минимизацию => обратная - на максимизацию
     bool dual_minimize = false;
 
-    size_t vars = general.num_constraints();
-    size_t constraints = general.num_variables();
+    size_t vars = problem.num_constraints();
+    size_t constraints = problem.num_variables();
 
 
     // c' = b
-    const std::vector<double>& dual_objective = general.rhs();
+    const std::vector<double>& dual_objective = problem.rhs();
 
     // A' = A^T
     std::vector<std::vector<double>> dual_constraints(constraints, std::vector<double>(vars, 0.0));
     for (size_t i = 0; i < vars; ++i) {
         for (size_t j = 0; j < constraints; ++j) {
-            dual_constraints[j][i] = general.constraints()[i][j];
+            dual_constraints[j][i] = problem.constraints()[i][j];
         }
     }
 
     // b' = c;
-    const std::vector<double>& dual_rhs = general.objective();
+    const std::vector<double>& dual_rhs = problem.objective();
 
 
-    const auto M1 = general.getM1();
-    const auto M2 = general.getM2();
-    const auto N1 = general.getN1();
-    const auto N2 = general.getN2();
+    const auto M1 = problem.getM1();
+    const auto M2 = problem.getM2();
+    const auto N1 = problem.getN1();
+    const auto N2 = problem.getN2();
 
-    std::vector<std::string> dual_relations(general.num_variables());
+    std::vector<std::string> dual_relations(problem.num_variables());
     for (const auto& i : N1) {
         dual_relations[i] = "<=";
     }
@@ -44,7 +45,7 @@ LinearProgram DualBuilder::build_dual(const LinearProgram& general) {
         dual_relations[i] = "=";
     }
 
-    std::vector<std::string> dual_var_constraints(general.num_constraints());
+    std::vector<std::string> dual_var_constraints(problem.num_constraints());
     for (const auto& i : M1) {
         dual_var_constraints[i] = ">=0";
     }
